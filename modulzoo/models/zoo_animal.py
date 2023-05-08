ea6dfd614ef9d06f82c0fb9a668dc54665e1cad3
Alex Man <alexandrumanmihai@gmail.com>
2023-05-08 20:41:32 +0200

    Moduls acabats

--- modulzoo/models/zoo_animal.py
+++ modulzoo/models/zoo_animal.py
@@ -1,6 +1,6 @@
 from odoo import models, fields     
-class zoo_editorial(models.Model): 
-    _name = 'zoo.editorial' 
+class zoo_animal(models.Model): 
+    _name = 'zoo.animal' 
     id = fields.Integer('ID')
     continentOrigen = fields.Char('Continent Origen', required=True) 
     dataNaix = fields.Date('Data Naixament')     
--- modulzoo/models/zoo_especie.py
+++ modulzoo/models/zoo_especie.py
@@ -1,9 +1,10 @@
 from odoo import models, fields     
-class zoo_editorial(models.Model): 
-    _name = 'zoo.editorial' 
-    nom = fields.Char('Nom', required=True) 
-    adreca = fields.Char('Adreça')     
-    telf = fields.Char('Telèfon')     
-    email = fields.Char('Correu electrònic')
+class zoo_especie(models.Model): 
+    _name = 'zoo.especie' 
+    id = fields.Integer('ID')
+    familia = fields.Char('Familia')     
+    nomCientific = fields.Char('Nom Cientific')     
+    nomVulgar = fields.Char('Nom Vulgar')
+    perill = fields.Char('Perill')
 
     
